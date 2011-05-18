674374bb82e40457f3d19fef24345395bb63ac83
Ross Lonstein <rlonstein@pobox.com>
2011-05-17 21:20:15 -0400

    fix typo; translate tile set generation and dict lookup

--- wordwhiz.clj
+++ wordwhiz.clj
@@ -8,7 +8,8 @@
 ;;;
 
 (ns com.lonsteins.wordwhiz
-  (:require (clojure.contrib))
+  (:require (clojure.contrib)
+            (clojure.contrib.seq-utils))
   (:import (java.util HashSet)
            (javax.swing JFrame JPanel JButton JTextField JLabel SwingUtilities))
   (:use (clojure.contrib miglayout swing-utils)))
@@ -17,41 +18,52 @@
 (def rack-size 16)
 (def max-word-length rack-size)
 (def dictfile
-  ;;FIXME
+  ;;FIXME: pick this up at load
   )
 
 (def tile-data
-  ((\E 1 12)
-   (\A 1 9)
-   (\I 1 9)
-   (\O 1 8)
-   (\N 1 6)
-   (\R 1 6)
-   (\T 1 6)
-   (\L 1 4)
-   (\S 1 4)
-   (\U 1 4)
-   (\D 2 4)
-   (\G 2 3)
-   (\B 3 2)
-   (\C 3 2)
-   (\M 3 2)
-   (\P 3 2)
-   (\F 4 2)
-   (\H 4 2)
-   (\V 4 2)
-   (\W 4 2)
-   (\Y 4 2)
-   (\K 5 1)
-   (\J 8 1)
-   (\X 8 1)
-   (\Q 10 1)
-   (\Z 10 1)))    ;; Scrabble(tm) also (\SPACE 0 2)
+  '((\E 1 12)
+    (\A 1 9)
+    (\I 1 9)
+    (\O 1 8)
+    (\N 1 6)
+    (\R 1 6)
+    (\T 1 6)
+    (\L 1 4)
+    (\S 1 4)
+    (\U 1 4)
+    (\D 2 4)
+    (\G 2 3)
+    (\B 3 2)
+    (\C 3 2)
+    (\M 3 2)
+    (\P 3 2)
+    (\F 4 2)
+    (\H 4 2)
+    (\V 4 2)
+    (\W 4 2)
+    (\Y 4 2)
+    (\K 5 1)
+    (\J 8 1)
+    (\X 8 1)
+    (\Q 10 1)
+    (\Z 10 1)))    ;; Scrabble(tm) also (\SPACE 0 2)
 
 (defn read-dict [fn]
   "Read a wordlist from a file, returning the newly created set"
   (let [dict (HashSet. 100000)]
     (with-open [reader (java.io.BufferedReader.
                         (java.io.FileReader. fn))]
-      (doseq [ln (line-seq reader)] (.add dict ln)))
+      (doseq [ln (line-seq reader)] (.add dict (.toUpperCase ln))))
     dict))
+
+
+(defn tileset []
+  "Return a shuffled set of letters (tiles)"
+  (shuffle (for [[letter _ freq] tile-data] (replicate freq letter))))
+
+
+(defn valid-word? [word dict]
+  "Check word against the dictionary"
+  ;; this is just to hide the implementation...
+  (.contains dict (.toUpper word)))
