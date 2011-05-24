4456cb13a01b62ce888242175c795711190248ae
Ross Lonstein <rlonstein@pobox.com>
2011-05-23 21:05:13 -0400

    add pivot artifacts to proj, fix deps

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.apache.pivot/pivot "2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.apache.pivot/pivot-core "2.0"]
+                 [org.apache.pivot/pivot-wtk "2.0"]
+                 [org.apache.pivot/pivot-wtk-terra "2.0"]]
   :jar-name "wordwhiz.jar"
   :warn-on-reflection true
   :main wordwhiz.clj.core
