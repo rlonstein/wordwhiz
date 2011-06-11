ba5420174aea5bc8d854809bde0b72ee015b7820
Ross Lonstein <rlonstein@pobox.com>
2011-06-10 23:52:07 -0400

    custom button classes w/stub listener attached

--- src/wordwhiz/clj/ui.clj
+++ src/wordwhiz/clj/ui.clj
@@ -7,38 +7,66 @@
 
 (ns wordwhiz.clj.ui
   (:import
+   (java.net URL)
    (org.apache.pivot)
-   (org.apache.pivot.beans BXMLSerializer)
-   (org.apache.pivot.wtk Application
+   (org.apache.pivot.beans BXMLSerializer
+                           Bindable)
+   (org.apache.pivot.util Resources
+                          ListenerList)
+   (org.apache.pivot.collections Map)
+   (org.apache.pivot.wtk Alert
+                         Application
                          Application$UncaughtExceptionHandler
                          ApplicationContext
-                         DesktopApplicationContext
                          BrowserApplicationContext
+                         Button
+                         ButtonPressListener
+                         Component
+                         DesktopApplicationContext
                          Frame
-                         Window
-                         Component))
-  (:gen-class
-   :implements [org.apache.pivot.wtk.Application]
-   :state state
-   :init init)
-)
+                         MessageType
+                         PushButton
+                         Window)))
 
 (def uidescfile (. ClassLoader getSystemResource "ui.bxml"))
-
-(defn -init []
-  [ [] (atom []) ])
+ 
+(gen-class
+ :name wordwhiz.clj.ui
+ :implements [org.apache.pivot.wtk.Application]
+ :state state
+ :init init)
 
 (defn -main [& args]
   "Entry point for application-style (desktop) execution"
   (. DesktopApplicationContext main (. (ClassLoader/getSystemClassLoader) loadClass "wordwhiz.clj.ui") (into-array String args)))
 
 (defn -startup [this display props]
-  (. (. (org.apache.pivot.beans.BXMLSerializer.) readObject uidescfile) open display))
+   (. (. (org.apache.pivot.beans.BXMLSerializer.) readObject uidescfile) open display))
 
 (defn -resume [this])
 
 (defn -suspend [this])
 
 (defn -shutdown [this optional]
-  false)
+   false)
 
+(defn -init []
+  [ [] (atom []) ])
+
+(gen-class
+ :name wordwhiz.clj.ui.BoardButton
+ :extends org.apache.pivot.wtk.PushButton
+ :post-init attach-listener
+ :prefix btn-)
+
+(gen-class
+ :name wordwhiz.clj.ui.ActionButton
+ :extends org.apache.pivot.wtk.PushButton
+ :post-init attach-listener
+ :prefix btn-)
+
+(defn btn-attach-listener [btn]
+  (.. btn
+      (getButtonPressListeners)
+      (add (proxy [ButtonPressListener] []
+             (buttonPressed [b] (println "Hello from",b,(. b getName)))))))
