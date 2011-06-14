267340f3c355b99b4c35bf369f05923b78a4c860
Ross Lonstein <rlonstein@pobox.com>
2011-06-14 00:28:11 -0400

    use clojure 1.2.1

--- project.clj
+++ project.clj
@@ -2,16 +2,15 @@
   :description "Wordwhiz - A letter tile game"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.pivot/pivot-core "2.0"]
                  [org.apache.pivot/pivot-wtk "2.0"]
                  [org.apache.pivot/pivot-wtk-terra "2.0"]
                  [jflac/jflac-codec "1.4.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [clojure-source "1.2.0"]]
+                     [clojure-source "1.2.1"]]
   :jar-name "wordwhiz.jar"
   :warn-on-reflection false
   :main wordwhiz.clj.ui
-  :aot [wordwhiz.clj.core
-        wordwhiz.clj.ui])
+  :aot [wordwhiz.clj.ui])
