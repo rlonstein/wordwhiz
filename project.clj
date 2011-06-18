70274393fa79269b143aec3561ff21f5e92231cd
Ross Lonstein <rlonstein@pobox.com>
2011-06-17 21:34:03 -0400

    remove unused contrib from deps

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.pivot/pivot-core "2.0"]
                  [org.apache.pivot/pivot-wtk "2.0"]
                  [org.apache.pivot/pivot-wtk-terra "2.0"]
                  [jflac/jflac-codec "1.4.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [clojure-source "1.2.1"]]
+;  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :jar-name "wordwhiz.jar"
   :warn-on-reflection false
   :main wordwhiz.clj.ui
