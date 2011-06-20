73994df42d1f3a883fdb3bc881946d1bb4fafc31
Ross Lonstein <rlonstein@pobox.com>
2011-06-20 19:57:06 -0400

    basic tests

--- test/wordwhiz/clj/test/core.clj
+++ test/wordwhiz/clj/test/core.clj
@@ -2,5 +2,45 @@
   (:use [wordwhiz.clj.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(def test-dict (read-dict dictfile))
+
+(deftest test-read-dict
+  (is (> (count test-dict) 1)))
+
+(def test-tiles (tileset))
+
+(deftest test-tileset
+  (is (= 98 (count (flatten test-tiles)))))
+
+(def test-board (fill-board test-tiles))
+
+(deftest test-fill-board
+  (is (vector? test-board)))
+
+(deftest test-tile-at
+  (are (not (nil? (tile-at test-board 0 0)))
+       (not (nil? (tile-at test-board (dec (:x board-dim)) (dec (:y board-dim)))))))
+
+(deftest test-valid-word
+  (are (true? (valid-word? "AARDVARK" test-dict))
+       (true? (valid-word? "ZYMURGY" test-dict))
+       (true? (not (valid-word? "FRUMIOUS" test-dict)))))
+
+(deftest test-score-word
+  (is (= (score-word "AARDVARK") 128)))
+
+(def test-game (new-game))
+
+;; (deftest test-new-game
+;;   (are (true? (:board test-game))
+;;        (true? (:tiles test-game))
+;;        (true? (:dictionary test-game))
+;;        (nil? (:history test-game))
+;;        (zero? (:score test-game))
+;;        (nil? (:rack test-game))))
+
+(deftest test-score-rack
+  (is (and
+       (zero? (:score (score-rack test-game)))
+       (= 128 (:score (score-rack (merge test-game {:rack [\A \A \R \D \V \A \R \K]})))))))
+
