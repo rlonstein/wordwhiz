ccdc34aa74edbfc3154308844f0c6331fde1af4a
Ross Lonstein <rlonstein@pobox.com>
2012-03-12 19:56:20 -0400

    explicitly add clojure-contrib to deps (broken with lein update)

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.pivot/pivot-core "2.0"]
                  [org.apache.pivot/pivot-wtk "2.0"]
                  [org.apache.pivot/pivot-wtk-terra "2.0"]
                  [jflac/jflac-codec "1.4.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [clojure-source "1.2.1"]]
+  :dev-dependencies [[clojure-source "1.2.1"]]
   :resources-path "resources"
 ;  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :jar-name "wordwhiz.jar"
