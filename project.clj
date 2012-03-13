8ce5d9002cff1109e0fb7eb638c3ac2ad78a4a20
Ross Lonstein <rlonstein@pobox.com>
2012-03-12 22:40:19 -0400

    use apache pivot r2.0.1

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.apache.pivot/pivot-core "2.0"]
-                 [org.apache.pivot/pivot-wtk "2.0"]
-                 [org.apache.pivot/pivot-wtk-terra "2.0"]
+                 [org.apache.pivot/pivot-core "2.0.1"]
+                 [org.apache.pivot/pivot-wtk "2.0.1"]
+                 [org.apache.pivot/pivot-wtk-terra "2.0.1"]
                  [jflac/jflac-codec "1.4.0-SNAPSHOT"]]
   :dev-dependencies [[clojure-source "1.2.1"]]
   :resources-path "resources"
